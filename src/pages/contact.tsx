f94a2a4d00cd3e6dd484e60375f52540e4d1d3de
onurravli <onurravli@hotmail.com>
2023-05-29 22:16:55 +0300

    feat: remove CommonHead and turn back to Head

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,19 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/Contact.module.css";
-import CommonHead from "@/shared/CommonHead";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Contact() {
 	return (
 		<>
-			<CommonHead title={"Onur Ravli | Contact"} />
+			<Head>
+				<title>Onur Ravli | Contact</title>
+				<meta name="description" content="Onur Ravli Personal Website" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="keywords" content="Onur Ravli, Onur Ravli Personal Website" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<main className={`${styles.main} ${inter.className}`}></main>
 		</>
 	);
